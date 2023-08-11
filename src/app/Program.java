31f3e706c5daf6223aae31a9c4a4662358874810
Spyke27 <rodrigoeufrasio2@gmail.com>
2023-08-11 13:46:28 -0300

    first commit

---
+++ src/app/Program.java
@@ -0,0 +1,16 @@
+package app;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Map<String, String> cookies = new TreeMap<>();
+		// <Chave, Valor>
+		
+
+	}
+
+}
